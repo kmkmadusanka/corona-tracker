5c2814278086e9e67b572290384dcd7ac3220621
kasun@speralabs.com <kasun@speralabs.com>
2020-05-26 17:40:50 +0530

    Updates

---
+++ precache-manifest.43ed225752a356d479e16c4a89d3534a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1a5d12137254275698d15e2b636d0c",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "2121fd845eb507fa26c3",
+    "url": "/corona-tracker/static/css/main.c65019ea.chunk.css"
+  },
+  {
+    "revision": "fbae804987b02e83eda2",
+    "url": "/corona-tracker/static/js/2.2f12fbfd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.2f12fbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2121fd845eb507fa26c3",
+    "url": "/corona-tracker/static/js/main.98a18545.chunk.js"
+  },
+  {
+    "revision": "325b7111eddbb9aa1ac5",
+    "url": "/corona-tracker/static/js/runtime-main.10fe4931.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
